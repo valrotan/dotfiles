123c46ba302f1d368ee08b692d37549b0188a6c8
Val Rotan <val.rotan@gmail.com>
2023-07-02 12:39:27 -0700

    changed neovim installation

--- .oh-my-zsh-custom/path.zsh
+++ .oh-my-zsh-custom/path.zsh
@@ -2,4 +2,4 @@ export PATH="$HOME/.local/bin":$PATH
 export PATH=/opt/homebrew/bin:$PATH
 export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 export PATH="$DOTFILES/.fzf/bin:$PATH"
-export PATH="$DOTFILES/neovim/bin:$PATH"
+export PATH="$DOTFILES/nvim/bin:$PATH"
--- install.sh
+++ install.sh
@@ -2,16 +2,39 @@
 
 set -e
 
+# setup repo
 DOTFILES=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $DOTFILES
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)
+        machine=Linux
+        nvim_url='https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz'
+        ;;
+    Darwin*)
+        nvim_url='https://github.com/neovim/neovim/releases/latest/download/nvim-macos.tar.gz'
+        machine=Mac
+        ;;
+    *)
+        machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
 
 git submodule update --init --recursive
 
 # setup nvim
-cd neovim
-make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$DOTFILES/neovim"
-make install
-cd ..
+# cd neovim
+# make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$DOTFILES/neovim"
+# make install
+# cd ..
+if [ -z $nvim_url ]; then
+    echo Could not find Neovim installation for $machine. Please install it yourself under nvim/;
+else
+    wget -O nvim.tar.gz $nvim_url
+    mkdir nvim && tar -xzf nvim.tar.gz -C nvim --strip-components 1
+    rm nvim.tar.gz
+fi
 
 # setup fzf
 .fzf/install --bin
