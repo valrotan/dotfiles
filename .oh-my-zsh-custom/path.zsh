36ffc1dffd3b94a69adf5eaf88b47e45350248f3
Val Rotan <val.rotan@gmail.com>
2023-10-10 16:23:39 -0700

    tmux updates

--- .oh-my-zsh-custom/path.zsh
+++ .oh-my-zsh-custom/path.zsh
@@ -3,3 +3,5 @@ export PATH=/opt/homebrew/bin:$PATH
 export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 export PATH="$DOTFILES/.fzf/bin:$PATH"
 export PATH="$DOTFILES/nvim/bin:$PATH"
+
+export TERMINFO_DIRS=$TERMINFO_DIRS:$HOME/.local/share/terminfo
