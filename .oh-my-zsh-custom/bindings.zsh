4b6c57eeaed72a6305275c2fbee9e77fd936f2d5
Val Rotan <valeriy.rotan@berkeley.edu>
2023-12-23 10:18:56 +0000

    zsh bindings

---
+++ .oh-my-zsh-custom/bindings.zsh
@@ -0,0 +1,7 @@
+bindkey '^h' backward-char
+bindkey '^l' forward-char
+bindkey '^j' end-of-line
+bindkey '^k' beginning-of-line
+bindkey '^w' forward-word
+bindkey '^b' backward-word
+bindkey '^ ' autosuggest-accept
