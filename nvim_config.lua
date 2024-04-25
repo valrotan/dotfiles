1bcafc6dfd7417fbf03f509063a3de4038493e52
Val Rotan <val.rotan@gmail.com>
2024-04-24 22:55:56 -0700

    disable illuminate for large files

--- nvim_config.lua
+++ nvim_config.lua
@@ -235,7 +235,7 @@ require('illuminate').configure({
 	under_cursor = true,
 	-- large_file_cutoff: number of lines at which to use large_file_config
 	-- The `under_cursor` option is disabled when this cutoff is hit
-	large_file_cutoff = nil,
+	large_file_cutoff = 10000,
 	-- Supports the same keys passed to .configure
 	-- If nil, vim-illuminate will be disabled for large files.
 	large_file_overrides = nil,
